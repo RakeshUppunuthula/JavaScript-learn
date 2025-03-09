dd759fce535415c94059d853bc4934d3d35982e4
RakeshUppunuthula <rakesh181000@gmail.com>
2025-03-09 16:26:40 +0000

    Arrays01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+const myArr =[1,2,3,4,5]
+const marvel =["IorMan","Thor","Panther"]
+
+
+
+const arr1 = new Array(1,2,3,4,5,6)
+console.log(arr1);
+
+//arr1.push(7)
+
+//arr1.pop()
+//arr1.unshift(9)
+//arr1.shift()
+//console.log(arr1.includes(9))
+//console.log(arr1.indexOf(3))
+//const newArr = arr1.join     //converts in to string type and binds
+console.log("a" ,arr1);
+
+const mya1 = arr1.slice(1,3)
+console.log(mya1)
+console.log("b",arr1)
+const mya2 =arr1.splice(1,3)
+console.log(mya2)
+console.log("c" ,arr1)
