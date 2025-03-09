683be0769824389d1e253aa4138eac704ec42804
RakeshUppunuthula <rakesh181000@gmail.com>
2025-03-09 07:42:54 +0000

    converstionOperartion 1commit

---
+++ 01_basics/03_converstionOperations.js
@@ -0,0 +1,38 @@
+let score ="33"
+
+console.log(typeof(score));
+let valueInNumber  = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+/* conb=verting Differnt datatYpes in to Number Test Case Scenarios
+
+"33" ==33   string to Number conversion
+"33a" == NAN
+"Rakesh" ==NaN
+ boolean  == true then 1 false then 0\
+ null == 0
+
+*/
+
+
+let LogedIn  = undefined
+console.log(LogedIn);
+
+let BooleanLogedIn = Boolean(LogedIn)
+console.log(BooleanLogedIn);
+
+
+ /* the convertion in datatype Boolean
+ 
+ if Value is 0 it is false other than any value in numbers is true 
+ if we enter any string it is true if it is empty string is false
+
+ null=> false
+ undefined => false
+
+ */
+
+
+
