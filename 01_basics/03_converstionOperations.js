4ae74f422ca1c8dad941b281dce243ed76fe616b
RakeshUppunuthula <rakesh181000@gmail.com>
2025-03-09 09:53:17 +0000

    ConverstionOperation SecondCommit

--- 01_basics/03_converstionOperations.js
+++ 01_basics/03_converstionOperations.js
@@ -35,4 +35,29 @@ console.log(BooleanLogedIn);
  */
 
 
+ //************************************** operation**********************************//
+
+let str1 = "Rakesh "
+let str2 ="Uppunuthula"
+
+let str3 =str1+str2
+console.log(str3);
+
+
+/* all ouputs of console are resulte 12 and type of result is String */
+console.log("1"+2);
+console.log("1"+"2");
+console.log(1+"2");
+
+
+
+/**`x++` (Post-increment):** First assigns the current value, then increments.  
+**`++x` (Pre-increment):** First increments, then assigns the new value. 
+  **Separate lines:** `x` is incremented before the next statement executes.  
+ **Assignment difference:** `x++` gives the old value, `++x` gives the new value immediately. */
+
+
+
+ 
+
 
