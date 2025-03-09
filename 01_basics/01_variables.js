94c2860a307638d79b9399d278620bf03721200c
RakeshUppunuthula <rakesh181000@gmail.com>
2025-03-09 06:04:01 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =144543
+let accountEmail = "Rakesh@google.com"
+var accountPassword ="12345"
+accountAddress = "Warangal"
+let accountState;
+
+// accountId =88  not allowed this  accountId is a constant
+/*
+
+prefer not to use Var because the issues in block scope and functional scope
+*/
+
+accountEmail ="rohith@google.com" 
+accountPassword = "97047"
+accountAddress ="Hyderabad"
+
+console.table([accountId,accountEmail,accountPassword,accountAddress,accountState])
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use Strict" ;//treat all code in newer js
+
+
+//alert("Hello") we are using Node js not Browser
+
+
+
+let name ="Rakesh"
+let age =24
+let isLoginIn =true
+
+console.table([name,age,isLoginIn])
+
+
+//number => 2 to power of 53
+//bigInt
+//string => ""
+// boolean => true/false
+// null => standole value
+//undefined => where variable declared but not values assigned
+// symbol => uniqueNess
+
+//objects
+console.log(typeof isLoginIn );
