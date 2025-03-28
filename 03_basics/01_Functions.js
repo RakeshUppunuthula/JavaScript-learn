e9a627c45dd35ee6ac8d07ef5295f465321fe943
RakeshUppunuthula <rakesh181000@gmail.com>
2025-03-28 10:43:37 +0000

    funtion

---
+++ 03_basics/01_Functions.js
@@ -0,0 +1,43 @@
+// function myName(){
+
+//     console.log("Hello")
+// }
+
+// myName();
+// myName
+
+// function addTwoNumbers(number1,number2){
+
+
+//     number3 = number1+number2
+//     console.log((number3));
+    
+// }
+
+//----------
+
+// function addTwoNumbers(number1,number2){
+
+//     let result =number1+number2
+
+//     return result
+// }
+
+// let result  = addTwoNumbers(2,3)
+
+// console.log(result);
+
+//--------------------------------------
+
+function caluculateCartPrice(...num1){
+    return num1
+}
+console.log(caluculateCartPrice(1,2,3,4,5));
+
+const user ={
+
+    naem:"Rakesh",
+    age:24
+}
+
+function handleObjects(){}
